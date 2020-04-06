c62b99901b383899f1b6e795c55b280815b933d8
Chen Xin <setsura3@gmail.com>
2020-04-06 17:17:06 -0400

    dict

---
+++ challengeo2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+col_list= ["blue", "Columbus"]
+col_list.append(1492)
+year = col_list[2]
+name = col_list[1]
+color = col_list[0]
+
+print('What is your name: ')
+user_name = input()
+print(f"In {year}, {name} sailed the ocean {color}. {user_name} fell off the boat.")
+
