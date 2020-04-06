18ea983859315fdf0cafa68514fe0233a3950105
Chen Xin <setsura3@gmail.com>
2020-04-06 12:55:22 -0400

    second

---
+++ chenfile.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+print("what is your name")
+name = input()
+a = str(name)
+print("What day it is today: ")
+day = input()
+print("Hello, " + name + "Hiappy " + day + " of the week!") 
